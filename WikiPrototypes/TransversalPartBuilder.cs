d8d302486a922a0d39c6d4f9a04038b85b19c0db
Andrei Chirea <chirea.andrei@gmail.com>
2024-05-01 20:56:14 +0200

    Add transversal structure builder

---
+++ WikiPrototypes/TransversalPartBuilder.cs
@@ -0,0 +1,59 @@
+﻿using Rhino.Geometry;
+
+namespace WikiPrototypes
+{
+    public static class TransversalPartBuilder
+    {
+        public static Curve GetContour()
+        {
+            var points = new Point3d[]
+            {
+                new Point3d(30.0, 2.5, 0),
+                new Point3d(27.6, 2.5, 0),
+                new Point3d(27.6, 3.7, 0),
+                new Point3d(28.2, 3.7, 0),
+                new Point3d(28.2, 12.5, 0),
+                new Point3d(7.20, 12.5, 0),
+                new Point3d(7.20, 11.9, 0),
+                new Point3d(6.00, 11.9, 0),
+                new Point3d(6.00, 14.3, 0),
+
+                new Point3d(-6.00, 14.3, 0),
+                new Point3d(-6.00, 11.9, 0),
+                new Point3d(-7.20, 11.9, 0),
+                new Point3d(-7.20, 12.5, 0),
+                new Point3d(-28.2, 12.5, 0),
+                new Point3d(-28.2, 3.7, 0),
+                new Point3d(-27.6, 3.7, 0),
+                new Point3d(-27.6, 2.5, 0),
+                new Point3d(-30.0, 2.5, 0),
+
+                new Point3d(-30.0, -2.5, 0),
+                new Point3d(-27.6, -2.5, 0),
+                new Point3d(-27.6, -3.7, 0),
+                new Point3d(-28.2, -3.7, 0),
+                new Point3d(-28.2, -12.5, 0),
+                new Point3d(-7.20, -12.5, 0),
+                new Point3d(-7.20, -11.9, 0),
+                new Point3d(-6.00, -11.9, 0),
+                new Point3d(-6.00, -14.3, 0),
+
+                new Point3d(6.00, -14.3, 0),
+                new Point3d(6.00, -11.9, 0),
+                new Point3d(7.20, -11.9, 0),
+                new Point3d(7.20, -12.5, 0),
+                new Point3d(28.2, -12.5, 0),
+                new Point3d(28.2, -3.7, 0),
+                new Point3d(27.6, -3.7, 0),
+                new Point3d(27.6, -2.5, 0),
+                new Point3d(30.0, -2.5, 0),
+
+                new Point3d(30.0, 2.5, 0),
+            };
+
+            var contour = new PolylineCurve(points);
+
+            return contour;
+        }
+    }
+}
